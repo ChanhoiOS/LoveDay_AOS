29b3f90144658a02c606feb653cb56385c3a0c90
chanho <lchh93@naver.com>
2023-08-16 14:29:25 +0900

    [AOS] 하단 탭바

--- app/src/main/java/com/chanho/loveday/MainActivity.kt
+++ app/src/main/java/com/chanho/loveday/MainActivity.kt
@@ -2,10 +2,27 @@ package com.chanho.loveday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.chanho.loveday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initBinding()
+        initNavigation()
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
     }
 }
