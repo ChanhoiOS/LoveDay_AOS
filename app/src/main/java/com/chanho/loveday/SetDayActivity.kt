8b42dfc0daef50cb2c741434e1ff4a02c41df67a
chanho <lchh93@naver.com>
2023-08-22 15:32:26 +0900

    기념일 구하기

--- app/src/main/java/com/chanho/loveday/SetDayActivity.kt
+++ app/src/main/java/com/chanho/loveday/SetDayActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import com.applikeysolutions.cosmocalendar.selection.OnDaySelectedListener
 import com.applikeysolutions.cosmocalendar.selection.SingleSelectionManager
 import com.chanho.loveday.databinding.ActivitySetDayBinding
+import java.text.SimpleDateFormat
 
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -25,7 +26,10 @@ class SetDayActivity : AppCompatActivity() {
             val month = binding.calendarView.selectedDays[0].calendar[2] + 1
             val day = binding.calendarView.selectedDays[0].dayNumber
 
+            val formattedDate = String.format("%04d-%02d-%02d", year, month, day)
+
             getDDay(year, month, day)
+            getSpecialInfo(formattedDate)
         })
     }
 
@@ -51,6 +55,76 @@ class SetDayActivity : AppCompatActivity() {
         val differenceInDays = TimeUnit.MILLISECONDS.toDays(differenceInMillis) + 1 // 사귄 첫 날을 1일로 처리합니다.
 
         Log.d("differenceInDays", differenceInDays.toString())
+    }
+
+    fun getSpecialInfo(dday: String) {
+        val dateFormatter = SimpleDateFormat("yyyy-MM-dd")
+        val startDate: Date = dateFormatter.parse(dday) ?: return
+
+        val calendar = Calendar.getInstance()
+        val specialDDayCheck: MutableList<Map<String, Int>> = mutableListOf()
+
+        // 100일 단위의 기념일 추출
+        for (day in 0..7200 step 100) {
+            val specialDate: Date
+            if (day == 0) {
+                specialDate = startDate
+            } else {
+                specialDate = Calendar.getInstance().apply {
+                    time = startDate
+                    add(Calendar.DAY_OF_YEAR, day - 1)
+                }.time
+            }
+
+            val specialStr = dateFormatter.format(specialDate)
+            specialDDayCheck.add(mapOf(specialStr to day))
+        }
+
+        // 1년 단위의 기념일 추출
+        for (year in 1..20) {
+            val anniversaryDate: Date = Calendar.getInstance().apply {
+                time = startDate
+                add(Calendar.YEAR, year)
+            }.time
+
+            val anniversaryStr = dateFormatter.format(anniversaryDate)
+            val days = year * 365
+            if (year in 1..3) {
+                val insertIndex = (year - 1) * 5 + year
+                specialDDayCheck.add(insertIndex, mapOf(anniversaryStr to days))
+            } else if (year in 4..20) {
+                specialDDayCheck.add(mapOf(anniversaryStr to days))
+            }
+        }
+
+        getValue(specialDDayCheck)
+    }
+
+    fun getValue(specialDay: List<Map<String, Int>>) {
+        val specialDate: MutableList<String> = mutableListOf()
+        val specialDayName: MutableList<String> = mutableListOf()
+
+        for (day in specialDay) {
+            for ((key, value) in day) {
+                specialDate.add(key)
+                when {
+                    value == 0 -> specialDayName.add("만난 날")
+                    value % 365 == 0 -> {
+                        val year = value / 365
+                        specialDayName.add("${year}주년")
+                    }
+                    else -> specialDayName.add("${value}일")
+                }
+            }
+        }
+
+        saveSpecialDay(specialDate, specialDayName)
+    }
+
+    fun saveSpecialDay(specialDate: List<String>, specialDayName: List<String>) {
+        // 저장 로직을 여기에 구현
+        Log.d("specialDate:: ", specialDate.toString())
+        Log.d("specialDayName:: ", specialDayName.toString())
     }
 }
 
