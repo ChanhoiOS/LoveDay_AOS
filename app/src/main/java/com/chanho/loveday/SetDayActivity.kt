640715e77257babee39f81202fc394dba4abcdbf
chanho <lchh93@naver.com>
2023-08-22 14:22:22 +0900

    선택된 날짜 출력

--- app/src/main/java/com/chanho/loveday/SetDayActivity.kt
+++ app/src/main/java/com/chanho/loveday/SetDayActivity.kt
@@ -2,13 +2,38 @@ package com.chanho.loveday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.applikeysolutions.cosmocalendar.selection.OnDaySelectedListener
+import com.applikeysolutions.cosmocalendar.selection.RangeSelectionManager
+import com.applikeysolutions.cosmocalendar.selection.SingleSelectionManager
+import com.chanho.loveday.databinding.ActivitySetDayBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class SetDayActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySetDayBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_day)
+        binding = ActivitySetDayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        val calendarView = findViewById<CalendarView>(R.id.calendarView)
-//        calendarView.dayBinder = DayViewContainer(calendarView)
+        binding.calendarView.selectionManager = SingleSelectionManager(OnDaySelectedListener {
+            val weekYear = binding.calendarView.selectedDays[0].calendar.weekYear
+            Log.d("selected: ", weekYear.toString())
+
+            val month = binding.calendarView.selectedDays[0].calendar[2] + 1
+            Log.d("month: ", month.toString())
+
+            val dayNumber = binding.calendarView.selectedDays[0].dayNumber
+            Log.d("dayNumber: ", dayNumber.toString())
+
+            val formattedDate = String.format("%04d-%02d-%02d", weekYear, month, dayNumber)
+            Log.d("formattedDate", formattedDate)
+
+        })
     }
 }
+
