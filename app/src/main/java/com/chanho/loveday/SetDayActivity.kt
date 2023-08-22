5e7e4546484e7cb105507a8d5835f405b1a12517
chanho <lchh93@naver.com>
2023-08-22 14:54:49 +0900

    코드 정리

--- app/src/main/java/com/chanho/loveday/SetDayActivity.kt
+++ app/src/main/java/com/chanho/loveday/SetDayActivity.kt
@@ -4,12 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.applikeysolutions.cosmocalendar.selection.OnDaySelectedListener
-import com.applikeysolutions.cosmocalendar.selection.RangeSelectionManager
 import com.applikeysolutions.cosmocalendar.selection.SingleSelectionManager
 import com.chanho.loveday.databinding.ActivitySetDayBinding
-import java.lang.Math.abs
-import java.text.SimpleDateFormat
-import java.time.LocalDate
+
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -25,41 +22,35 @@ class SetDayActivity : AppCompatActivity() {
 
         binding.calendarView.selectionManager = SingleSelectionManager(OnDaySelectedListener {
             val year = binding.calendarView.selectedDays[0].calendar.weekYear
-            Log.d("selected: ", year.toString())
-
             val month = binding.calendarView.selectedDays[0].calendar[2] + 1
-            Log.d("month: ", month.toString())
-
             val day = binding.calendarView.selectedDays[0].dayNumber
-            Log.d("dayNumber: ", day.toString())
 
-            val formattedDate = String.format("%04d-%02d-%02d", year, month, day)
-            Log.d("formattedDate", formattedDate)
-
-
-            val selectedCalendar = Calendar.getInstance().apply {
-                set(Calendar.YEAR, year)
-                set(Calendar.MONTH, month - 1) // 0부터 시작하는 월을 반영하여 -1 해줍니다.
-                set(Calendar.DAY_OF_MONTH, day)
-                set(Calendar.HOUR_OF_DAY, 0)
-                set(Calendar.MINUTE, 0)
-                set(Calendar.SECOND, 0)
-                set(Calendar.MILLISECOND, 0)
-            }
-
-            val today = Calendar.getInstance().apply {
-                set(Calendar.HOUR_OF_DAY, 0)
-                set(Calendar.MINUTE, 0)
-                set(Calendar.SECOND, 0)
-                set(Calendar.MILLISECOND, 0)
-            }
-
-            val differenceInMillis = today.timeInMillis - selectedCalendar.timeInMillis
-            val differenceInDays = TimeUnit.MILLISECONDS.toDays(differenceInMillis) + 1 // 사귄 첫 날을 1일로 처리합니다.
-
-            Log.d("differenceInDays", differenceInDays.toString())
-
+            getDDay(year, month, day)
         })
+    }
+
+    fun getDDay(year: Int, month: Int, day: Int) {
+        val selectedCalendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month - 1) // 0부터 시작하는 월을 반영하여 -1 해줍니다.
+            set(Calendar.DAY_OF_MONTH, day)
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        val today = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        val differenceInMillis = today.timeInMillis - selectedCalendar.timeInMillis
+        val differenceInDays = TimeUnit.MILLISECONDS.toDays(differenceInMillis) + 1 // 사귄 첫 날을 1일로 처리합니다.
+
+        Log.d("differenceInDays", differenceInDays.toString())
     }
 }
 
