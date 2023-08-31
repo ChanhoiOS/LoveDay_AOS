44f05653fe8d001f4e9cbbed9fe666e6ef02ae43
chanho <lchh93@naver.com>
2023-08-31 10:22:18 +0900

    [AOS] SettingView 수정

--- app/src/main/java/com/chanho/loveday/SettingActivity.kt
+++ app/src/main/java/com/chanho/loveday/SettingActivity.kt
@@ -1,37 +1,45 @@
 package com.chanho.loveday
 
 import android.app.Activity
+import android.app.Application
 import android.content.*
 import android.os.Bundle
 import android.view.View.OnLongClickListener
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.chanho.loveday.application.MyApplication
 import com.chanho.loveday.databinding.ActivitySettingBinding
 
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
-    private var preferences: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.settingCloseButton.setOnClickListener {
-            val resultIntent = Intent()
-            // 결과 데이터 추가
-            setResult(Activity.RESULT_OK, resultIntent)
-            finish() // 현재 액티비티 종료
-        }
+        initView()
+        setBtnEvent()
+        setClipboard()
+    }
 
-        preferences = getSharedPreferences("setDDay", MODE_PRIVATE);
-
-        val privateKey = preferences?.getString("privateKey", "") ?: ""
+    private fun initView() {
+        val privateKey = MyApplication.prefs.getString("privateKey", "")
 
         binding.privateCodeLabelText.text = privateKey
+    }
 
+    private fun setBtnEvent() {
+        binding.settingCloseButton.setOnClickListener {
+            val resultIntent = Intent()
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+
+    private fun setClipboard() {
         binding.privateCodeLabelText.setOnClickListener {
             val textToCopy = binding.privateCodeLabelText.text.toString()
 
@@ -43,13 +51,6 @@ class SettingActivity : AppCompatActivity() {
 
             // 클립보드에 데이터를 복사
             clipboardManager.setPrimaryClip(clipData)
-
-            // 복사되었다는 메시지 또는 피드백을 사용자에게 표시할 수 있음
-            // 예를 들어 Toast 메시지를 사용하여 사용자에게 알릴 수 있음
-             Toast.makeText(this, "텍스트가 클립보드에 복사되었습니다.", Toast.LENGTH_SHORT).show()
         }
     }
-
-
-
 }
