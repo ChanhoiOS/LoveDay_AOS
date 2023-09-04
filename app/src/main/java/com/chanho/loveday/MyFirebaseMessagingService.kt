692ba4ee2b35c0e2f7089ffe94b0b01c8cdffcf5
chanho <lchh93@naver.com>
2023-09-04 16:32:45 +0900

    [AOS] 푸시데이터 NULL 제거

--- app/src/main/java/com/chanho/loveday/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/chanho/loveday/MyFirebaseMessagingService.kt
@@ -80,8 +80,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // 알림에 대한 UI 정보, 작업
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
-            .setContentTitle(remoteMessage.data["title"].toString()) // 제목
-            .setContentText(remoteMessage.data["body"].toString()) // 메시지 내용
+            .setContentTitle("상대방이 새 글을 등록했습니다.") // 제목
+            .setContentText("새 글을 확인해보세요!") // 메시지 내용
             .setAutoCancel(true) // 알람클릭시 삭제여부
             .setSound(soundUri)  // 알림 소리
             .setContentIntent(pendingIntent) // 알림 실행 시 Intent
